356ec7213f082abbf4d57c1a15f09f1e96829091
Daniel Niko <daniel.gultom@gmail.com>
2019-03-28 10:08:24 +0700

    Initial commit

---
+++ src/main/java/com/danielniko/crudbudget/CrudbudgetApplication.java
@@ -0,0 +1,13 @@
+package com.danielniko.crudbudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudbudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudbudgetApplication.class, args);
+	}
+
+}
